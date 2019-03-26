7ea357e65079f0d1031b124d430c00c5c52b3d81
cristiano giulioni <cristiano.giulioni@nautes.com>
2019-03-26 14:14:51 +0100

    Simplest swagger for WebApi Core

---
+++ SwaggerWebApi/SwaggerWebApi/CustomDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerWebApi
+{
+    public class CustomDto
+    {
+        public int Field1 { get; set; }
+        public string Field2 { get; set; }
+    }
+}
